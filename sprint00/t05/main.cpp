6d41b174cc67f06f3f4c337930fede8e3ad17a2d
Tetiana Rohalska <55732200+trohalska@users.noreply.github.com>
2020-08-26 17:33:46 +0300

    spr00 t05

--- sprint00/t05/main.cpp
+++ sprint00/t05/main.cpp
@@ -2,42 +2,28 @@
 
 int main(int argc, char *argv[]) {
 
-	if (argc < 5) 
-		std::cerr << "usage:./automaton [name] [level] [health] [stamina]";
-	auto name = argv[1];
-	auto level = argv[2];
-	auto health = argv[3];
-	auto stamina = argv[4];
+    if (5 != argc) {
+        std::cerr << "usage:./automaton [name] [level] [health] [stamina]";
+        return EXIT_FAILURE;
+    }
 
-	if (typeid(level) != typeid(int)) {
-		std::cerr << "Invalid argument: " << level;
-		return 1;
-	}
-	if (typeid(health) != typeid(float)) {
-		std::cerr << "Invalid argument: " << health;
-		return 1;
-	}
-	if (typeid(stamina) != typeid(float)) {
-		std::cerr << "Invalid argument: " << stamina;
-		return 1;
-	}
+    int i;
+    size_t ind;
+    try {
+        for (i = 2; i < 5; ++i) {
+            if (2 == i) std::stoi(std::string(argv[i]), &ind);
+            else if (3 == i) std::stof(std::string(argv[i]), &ind);
+            else if (4 == i) std::stod(std::string(argv[i]), &ind);
 
-	std::cout << "Name = " << name << std::endl
-			  << "Level = " << level << std::endl
-  			  << "Health = " << health << std::endl
-  			  << "Stamina = " << stamina << std::endl;
-  	return 0;
+            if (argv[i][ind] != '\0') throw argv[i];
+        }
+        std::cout << "Name = " << argv[1]
+                << "\nLevel = " << argv[2]
+                << "\nHealth = " << argv[3]
+                << "\nStamina = " << argv[4] << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Invalid argument: " << argv[i] << std::endl;
+    }
+    return 0;
 }
-
-// std::string arg = argv[1];
-// try {
-//   std::size_t pos;
-//   int x = std::stoi(arg, &pos);
-//   if (pos < arg.size()) {
-//     std::cerr << "Trailing characters after number: " << arg << '\n';
-//   }
-// } catch (std::invalid_argument const &ex) {
-//   std::cerr << "Invalid argument: " << arg << '\n';
-// } catch (std::out_of_range const &ex) {
-//   std::cerr << "Number out of range: " << arg << '\n';
-// }
